03ba44325ce5bca7d4266f95b5ca47951337596d
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-02-14 13:17:03 -0500

    Using Select (drop down) for calculation

--- js/app.js
+++ js/app.js
@@ -3,23 +3,51 @@ var myapp = angular.module('sample', []);
 myapp.controller('CtrlOne', ['$scope', function($scope) {
     console.log('Hello');
 
-    $scope.add = function() {
-        console.log('add');
-        $scope.result = +$scope.num1 + +$scope.num2;
-    };
+    $scope.bNames = [{
+        name: 'Add'
+    }, {
+        name: 'Sub'
+    }, {
+        name: 'Mul'
+    }, {
+        name: 'Div'
+    }];
 
-    $scope.sub = function() {
-        console.log('sub');
-        $scope.result = $scope.num1 - $scope.num2;
-    };
+    /* $scope.add = function() {
+         console.log('add');
+         $scope.result = +$scope.num1 + +$scope.num2;
+     };
 
-    $scope.div = function() {
-        console.log('div');
-        $scope.result = $scope.num1 / $scope.num2;
-    };
+     $scope.sub = function() {
+         console.log('sub');
+         $scope.result = $scope.num1 - $scope.num2;
+     };
 
-    $scope.mul = function() {
-        console.log('mul');
-        $scope.result = $scope.num1 * $scope.num2;
-    };
+     $scope.div = function() {
+         console.log('div');
+         $scope.result = $scope.num1 / $scope.num2;
+     };
+
+     $scope.mul = function() {
+         console.log('mul');
+         $scope.result = $scope.num1 * $scope.num2;
+     };*/
+
+    $scope.calculate = function() {
+        console.log($scope.operation);
+        if ($scope.operation.name == "Add") {
+            console.log('add');
+            $scope.result = +$scope.num1 + +$scope.num2;
+        } else if ($scope.operation.name == "Sub") {
+            console.log('sub');
+            $scope.result = $scope.num1 - $scope.num2;
+        } else if ($scope.operation.name == "Mul") {
+            console.log('mul');
+            $scope.result = $scope.num1 * $scope.num2;
+        } else if ($scope.operation.name == "Div") {
+            console.log('div');
+            $scope.result = $scope.num1 / $scope.num2;
+
+        }
+    }
 }]);
