d3eb61a932b3503d8a6d892867d2b57d2fb874c2
Jagdeep Matharu <Jagdeepmatharu88@gmail.com>
2015-02-14 12:55:42 -0500

    Fixing app function to add digits

--- js/app.js
+++ js/app.js
@@ -5,18 +5,21 @@ myapp.controller('CtrlOne', ['$scope', function($scope) {
 
     $scope.add = function() {
         console.log('add');
-        $scope.result = $scope.num1 + $scope.num2;
+        $scope.result = +$scope.num1 + +$scope.num2;
     };
 
     $scope.sub = function() {
+        console.log('sub');
         $scope.result = $scope.num1 - $scope.num2;
     };
 
     $scope.div = function() {
+        console.log('div');
         $scope.result = $scope.num1 / $scope.num2;
     };
 
     $scope.mul = function() {
+        console.log('mul');
         $scope.result = $scope.num1 * $scope.num2;
     };
 }]);
